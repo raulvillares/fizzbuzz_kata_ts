b028514fab8e08b205f2be928b17932561c44461
salite <salite@voxelgroup.net>
2022-01-26 10:30:23 +0100

    feat: return buzz when number is 5

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -3,6 +3,9 @@ export class FizzBuzz {
         if(n === 3){
             return "fizz";
         }
+        if (n === 5 ) {
+            return "buzz";
+        }
         return n.toString();
     }
 }
