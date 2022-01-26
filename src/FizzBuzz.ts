8fac468751361b29dcdac8c83a3601850dbdb4cd
salvarez <salvarez@voxelgroup.net>
2022-01-26 10:57:26 +0100

    feat: when number is multiple of 5 returns buzz

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,9 +1,9 @@
 export class FizzBuzz {
     generate(n: number) :string {
-        if(n%3 == 0){
+        if(n%3 === 0){
             return "fizz";
         }
-        if (n === 5 ) {
+        if (n%5 === 0) {
             return "buzz";
         }
         return n.toString();
