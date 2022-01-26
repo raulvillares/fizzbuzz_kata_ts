7aa3dfe7c541152fbc695708dae760a9ee989ef9
Raúl Villares <raulvillares@gmail.com>
2022-01-26 09:59:43 +0100

    Delete js file

--- src/FizzBuzz.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FizzBuzz = void 0;
-class FizzBuzz {
-    generate(n) {
-        return n.toString();
-    }
-}
-exports.FizzBuzz = FizzBuzz;
